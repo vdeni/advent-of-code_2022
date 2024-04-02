4319c896077421bae746b00ceb87afade25390be
Denis Vlašiček <30746378+vdeni@users.noreply.github.com>
2024-04-02 15:52:44 +0200

    07_day: add solution for part 1

---
+++ 07_day/no-space-left/src/main.rs
@@ -0,0 +1,208 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+
+fn main() {
+    let data_path = Path::new("data/data.txt");
+
+    let data_lines = read_input(data_path);
+
+    let lines_parsed: Vec<LineType> = data_lines.map(|ln| parse_line(ln.unwrap())).collect();
+
+    let file_tree = populate_file_tree(lines_parsed);
+
+    let dir_sizes = calculate_total_directory_sizes(file_tree);
+
+    let total_size = dir_sizes
+        .iter()
+        .filter(|dir| {
+            if dir.1 <= &100000 {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        .fold(0, |accumulator, elem| accumulator + elem.1);
+
+    println!("{total_size}");
+}
+
+fn read_input(file_path: &Path) -> io::Lines<BufReader<File>> {
+    let data_file = File::open(file_path).unwrap();
+    return BufReader::new(data_file).lines();
+}
+
+fn parse_line(cli_line: String) -> LineType {
+    /*!
+     * Parses the input line and returns a structured object representing the
+     * line's contents. May be a command or command output.
+     */
+
+    // determine whether the output line is a command or command output
+    let line_elems: Vec<String> = cli_line.split_whitespace().map(|x| x.to_string()).collect();
+
+    let line_elem_parsed: LineType = match line_elems[0].as_str() {
+        "$" => match line_elems[1].as_str() {
+            "cd" => LineType::CMD(Command::CD(line_elems[2].clone())),
+            "ls" => LineType::CMD(Command::LS),
+            _ => panic!("Unknown CMD provided in input: {}", line_elems[0].as_str()),
+        },
+        "dir" => LineType::OUT(CommandOut::DIR(FSDir {
+            name: line_elems[1].clone(),
+        })),
+        _ => LineType::OUT(CommandOut::FIILE(FSFile {
+            name: line_elems[1].clone(),
+            size: line_elems[0].parse::<usize>().unwrap(),
+        })),
+    };
+
+    return line_elem_parsed;
+}
+
+fn populate_file_tree(cli_lines: Vec<LineType>) -> HashMap<String, FSTree> {
+    let mut file_tree: HashMap<String, FSTree> = HashMap::new();
+    file_tree.insert(
+        String::from("/"),
+        FSTree {
+            children: HashMap::new(),
+            fs_elem: FSDir {
+                name: String::from("/"),
+            },
+        },
+    );
+
+    let mut current_path: Vec<String> = Vec::new();
+    // current_path.push(String::from("/"));
+
+    let mut current_dir = file_tree.get_mut("/").unwrap();
+
+    for line in cli_lines {
+        match line {
+            LineType::CMD(cmd) => match cmd {
+                Command::CD(dir) => {
+                    if dir == String::from("..") {
+                        current_path.pop();
+                        current_dir = file_tree.get_mut(&current_path.join("/")).unwrap();
+                    } else {
+                        current_path.push(dir.clone());
+                        file_tree.insert(
+                            current_path.join("/").clone(),
+                            FSTree {
+                                children: HashMap::new(),
+                                fs_elem: FSDir { name: dir.clone() },
+                            },
+                        );
+                        current_dir = file_tree.get_mut(&current_path.join("/")).unwrap();
+                    }
+                }
+                Command::LS => {
+                    continue;
+                }
+            },
+            LineType::OUT(out) => match out {
+                CommandOut::DIR(dir) => {
+                    current_dir
+                        .children
+                        .insert(dir.name.clone(), FSElem::Dir(dir));
+                }
+                CommandOut::FIILE(file) => {
+                    current_dir
+                        .children
+                        .insert(file.name.clone(), FSElem::File(file));
+                }
+            },
+        }
+    }
+
+    return file_tree;
+}
+
+fn calculate_directory_size(
+    directory_name: String,
+    directory: &FSTree,
+    fs_map: &HashMap<String, FSTree>,
+) -> usize {
+    let mut dir_size = 0;
+
+    for (_fs_elem_name, fs_elem) in &directory.children {
+        match fs_elem {
+            FSElem::File(file) => {
+                dir_size += file.size;
+            }
+            FSElem::Dir(dir) => {
+                // TODO: ovo je krivo. izrada target path ne valja, ali osim toga,
+                // ovako će iterirati samo po trivijalnim pathovima (/a, /d), ali
+                // ne i po dubljima. iterirati po fs_map?
+                // cijeli ovaj dio ne radi jer se sad u fs_map spremaju imena poput
+                // //a/b, a direktoriji su "a"
+                let mut target_path = directory_name.clone();
+                target_path.push_str("/");
+                target_path.push_str(&dir.name);
+
+                let target_dir = fs_map.get(&target_path).unwrap();
+                let target_dir_size = calculate_directory_size(target_path, target_dir, fs_map);
+                dir_size += target_dir_size;
+            }
+        }
+    }
+
+    return dir_size;
+}
+
+fn calculate_total_directory_sizes(file_tree: HashMap<String, FSTree>) -> HashMap<String, usize> {
+    /*!
+     * Calculate the total size for each directory.
+     */
+
+    let mut dir_sizes: HashMap<String, usize> = HashMap::new();
+
+    for (dir_name, dir) in &file_tree {
+        // možda dodati da se i ovaj dir_name šalje?
+        let dirsize = calculate_directory_size(dir_name.clone(), dir, &file_tree);
+        dir_sizes.insert(dir_name.clone(), dirsize);
+    }
+
+    return dir_sizes;
+}
+
+#[derive(Debug)]
+struct FSFile {
+    name: String,
+    size: usize,
+}
+
+#[derive(Debug)]
+struct FSDir {
+    name: String,
+}
+
+#[derive(Debug)]
+enum FSElem {
+    File(FSFile),
+    Dir(FSDir),
+}
+
+#[derive(Debug)]
+enum Command {
+    LS,
+    CD(String),
+}
+
+#[derive(Debug)]
+enum CommandOut {
+    DIR(FSDir),
+    FIILE(FSFile),
+}
+
+#[derive(Debug)]
+enum LineType {
+    CMD(Command),
+    OUT(CommandOut),
+}
+
+#[derive(Debug)]
+struct FSTree {
+    children: HashMap<String, FSElem>,
+    fs_elem: FSDir,
+}
